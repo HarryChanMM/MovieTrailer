67bf49cb3fd2037c150deeabeb7fdaec00b89d8e
Harry Chan <harrychan2139@gmail.com>
2016-11-13 16:15:12 -0500

    Resubmit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+#Fresh Tomatoes Movie Trailers
+
+## Setup
+1. Install Python in your computer.
+2. Run `python fresh_tomatoes.py` in the terminal.
+3. fresh_tomatoes.html is appear in the folder.
+4. Open fresh_tomatoes.html under the folder.
+Complete
+
+## Help
+You can contact to harrychan2139@gmail.com
+
+## About
+This is the tutorial of Udacity.com
+The learner is Harry Chan
--- entertainment_center.py
+++ entertainment_center.py
@@ -2,40 +2,49 @@ import fresh_tomatoes
 import media
 
 wonder_woman = media.Movie("Wonder Woman",
-                        "An Amazonian princess leaves her island home to explore the world and, in doing so, becomes one of the world's greatest heroes.",
+                        "An Amazonian princess leaves her island home to explore the"
+                        "world and, in doing so, becomes one of the world's greatest heroes.",
                         "http://t3.gstatic.com/images?q=tbn:ANd9GcTax3jGvMVAFVa_duRg6e2B8hKGVWLujhUE7kwrz0qmGORZZBWS",
                         "https://www.youtube.com/watch?v=5HUlW21v1fQ")
 
 valerian = media.Movie("Valerian",
-                       "Time-traveling agent Valerian is sent to investigate a galactic empire, along with his partner Laureline.",
+                       "Time-traveling agent Valerian is sent to investigate a galactic "
+                       "empire, along with his partner Laureline.",
                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwMzA2ODUwNl5BMl5BanBnXkFtZTgwNjk0OTQyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
                        "https://www.youtube.com/watch?v=RArJ8_WyfVs")
 
 guardians = media.Movie("Guardians of the Galaxy",
-                        "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.",
+                        "A group of intergalactic criminals are forced to work together "
+                        "to stop a fanatical warrior from taking control of the universe.",
                         "http://t2.gstatic.com/images?q=tbn:ANd9GcQW3LbpT94mtUG1PZIIzJNxmFX399wr_NcvoppJ82k7z99Hx6in",
                         "https://www.youtube.com/watch?v=wX0aiMVvnvg")
 
 power = media.Movie("Power Rangers",
-                        "A group of high-school kids, who are infused with unique superpowers, harness their abilities in order to save the world.",
+                        "A group of high-school kids, who are infused with unique superpowers, "
+                        "harness their abilities in order to save the world.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNDA3MTk3NzE0MF5BMl5BanBnXkFtZTgwOTYxNTQyOTE@._V1_SY1000_CR0,0,648,1000_AL_.jpg",
                         "https://www.youtube.com/watch?v=6CKtFztnJ9M")
 
 run = media.Movie("Run the Tide",
-                        "When their drug abusing mother is released from prison determined to rebuild their family, Rey kidnaps his younger brother Oliver and escapes their desert home for the California coast.",
+                        "When their drug abusing mother is released from prison determined to "
+                        "rebuild their family, Rey kidnaps his younger brother Oliver and escapes "
+                        "their desert home for the California coast.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMyNjIyMDg3MV5BMl5BanBnXkFtZTgwNjYyNTg0MDI@._V1_UX182_CR0,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=6GlYu09cxgY")
 
 justice = media.Movie("Justice League",
-                        "Fueled by his restored faith in humanity and inspired by Superman's selfless act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face an even greater enemy.",
+                        "Fueled by his restored faith in humanity and inspired by Superman's selfless "
+                        "act, Bruce Wayne enlists the help of his newfound ally, Diana Prince, to face "
+                        "an even greater enemy.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BYTk1ZTVmZmQtNDY2YS00MjRiLWFiOTQtNzg0MjFhNjVlMDYwXkEyXkFqcGdeQXVyNjY1MDU0OTM@._V1_UY268_CR150,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=fIHH5-HVS9o")
 
 blind = media.Movie("My Blind Brother",
-                        "The rivalry between two brothers reaches a fever pitch during a charity swim competition.",
+                        "The rivalry between two brothers reaches a fever pitch during a charity swim "
+                        "competition.",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTczNDE2NTk5N15BMl5BanBnXkFtZTgwMDMxOTM4OTE@._V1_UY268_CR2,0,182,268_AL_.jpg",
                         "https://www.youtube.com/watch?v=5jCQBCfJGhY")
 
-movies = [wonder_woman,valerian,guardians,power,run,justice,blind]
+movies = [wonder_woman, valerian, guardians, power, run, justice, blind]
 fresh_tomatoes.open_movies_page(movies)
 
